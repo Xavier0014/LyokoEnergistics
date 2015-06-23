da2d748d7496cc1e510d11f899b1973e3efc62c1
Xavier0014 <Xavbinet@videotron.ca>
2015-06-23 15:08:22 -0400

    numb 2

---
+++ src/main/java/com/xavier0014/lyokoenergistics/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.xavier0014.lyokoenergistics.reference;
+
+public class Reference {
+	
+	public static final String MOD_ID = "LyokoEnergistics";
+	public static final String MOD_NAME = "Lyoko Energistics";
+	public static final String MOD_VERSION = "LyokoEnergistics";
+
+}
